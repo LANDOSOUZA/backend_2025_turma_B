8c3d1bdb2d68b193be22cfce455a36a914b2e776
LANDOSOUZA <lando-souza@hotmail.com>
2025-08-13 20:33:55 -0300

    aula_2
    Exemplo Olá World - em Java Script

---
+++ aula_2/ex1.js
@@ -0,0 +1,7 @@
+// Primeiro exemplo de código em Java Script
+
+const idade = 29; // declara uma constante chamada  idade
+const nome = "Lando"; // declara uma constante chamada nome
+
+// imprime uma mnsagem no teminal
+console.log('Olá, Lando!')
