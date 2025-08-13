513bdbb624a8dbd79e63e72f61046fb17c542afa
LANDOSOUZA <lando-souza@hotmail.com>
2025-08-13 20:42:03 -0300

    aula-2
    Valor de variáveis no terminal

--- aula_2/ex1.js
+++ aula_2/ex1.js
@@ -5,3 +5,6 @@ const nome = "Lando"; // declara uma constante chamada nome
 
 // imprime uma mnsagem no teminal
 console.log('Olá, Lando!')
+
+//Exibindo o valor das constantes no terminal
+console.log(`${nome} idade ${idade}`); // Exibe o valor das constantes no terminal.
