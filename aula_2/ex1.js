e7b0a334453391a9fcf45e2a8f208c06130d6215
LANDOSOUZA <lando-souza@hotmail.com>
2025-08-13 20:51:49 -0300

    aula_2
    Exibindo mensagens concatenadas no terminal

--- aula_2/ex1.js
+++ aula_2/ex1.js
@@ -2,9 +2,12 @@
 
 const idade = 29; // declara uma constante chamada  idade
 const nome = "Lando"; // declara uma constante chamada nome
+const inf = nome + " " + idade; // Junta o nome e a idade
 
 // imprime uma mnsagem no teminal
 console.log('Olá, Lando!')
 
+
 //Exibindo o valor das constantes no terminal
 console.log(`${nome} idade ${idade}`); // Exibe o valor das constantes no terminal.
+console.log(inf); // Exibe o nome e a idade no terminal.
