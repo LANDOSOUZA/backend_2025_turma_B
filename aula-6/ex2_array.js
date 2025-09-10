c6bb56bc5b99fddb974112ac930eb75ccbbda47c
LANDOSOUZA <lando-souza@hotmail.com>
2025-09-10 20:36:14 -0300

    aula-6
    Exemplos de arrays

---
+++ aula-6/ex2_array.js
@@ -0,0 +1,12 @@
+// exemploo 2 com array
+notas = [5, 10, 7.5];
+console.log(notas);// exibe a lista de notas
+
+notas.pop(); //remove a última nota do array
+console.log(notas);
+
+notas.push(9); // adiciona uma nota no final do array
+console.log(notas);
+
+notas.push(10);
+console.log(notas); // adiciona uma nota no final do array
