e079f5178d499a069a9aaf08f46dac38c7dc4191
LANDOSOUZA <lando-souza@hotmail.com>
2025-09-24 19:40:49 -0300

    aula-6
    Commit - exercício 1.

---
+++ aula-6/exercicios/exercicio1.js
@@ -0,0 +1,11 @@
+// Crie um programa em Node.js que inicie com um array de cinco elementos e atribua 
+// um novo valor para a primeira posição (índice 0) desse array. Em seguida, 
+// exiba o array antes e depois da alteração.
+
+let elementos = [1, 2, 3, 4, 5]; // criação de um array com cinco elementos
+console.log("Array antes da alteração:", elementos); // exibe o array antes da alteração
+
+elementos[0] = 10; // atribui um novo valor para a primeira posição (índice 0)
+console.log("Array depois da alteração:", elementos); // exibe o array depois da alteração
+
+
