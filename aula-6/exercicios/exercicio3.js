0219c9f1767ac86935edf8c15cb2a23463da9b59
LANDOSOUZA <lando-souza@hotmail.com>
2025-09-24 19:47:39 -0300

    aula-6
    commit - exercício 3

---
+++ aula-6/exercicios/exercicio3.js
@@ -0,0 +1,9 @@
+// Desenvolva um programa em Node.js que crie um array vazio e atribua valores a ele utilizando o método push(). Adicione três
+// números inteiros ao array e, em seguida, exiba o array resultante.
+
+let numeros = []; // criação de um array vazio
+numeros.push(3); // adiciona o primeiro número inteiro
+numeros.push(7); // adiciona o segundo número inteiro
+numeros.push(11); // adiciona o terceiro número inteiro
+
+console.log("Array resultante:", numeros); // exibe o array resultante
