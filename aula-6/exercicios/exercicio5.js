52642e73e0de539ee351d1feea709a61c58565b0
LANDOSOUZA <lando-souza@hotmail.com>
2025-09-24 20:09:32 -0300

    aula-6
    Commit - exercício 6

---
+++ aula-6/exercicios/exercicio5.js
@@ -0,0 +1,23 @@
+// Crie um arquivo chamado livro.js. Nesse arquivo, crie um objeto livro que represente as informações de um
+// livro. O objeto deve conter as seguintes propriedades:
+
+// titulo (string): título do livro.
+// autor (string): nome do autor do livro.
+// anoPublicacao (number): ano de publicação do livro.
+// genero (string): gênero do livro.
+
+// No final do arquivo livro.js, instancie o objeto livro com dados de algum livro que você goste e imprima os
+// detalhes do livro no console.
+
+let livro = {
+  titulo: "Lando, O Mestre do Tabuleiro",
+  autor: "L. C. S. Souza",
+  anoPublicacao: 2027,
+  genero: "Raciocínio Lógico",
+};
+
+console.log("Detalhes do livro:");
+console.log("Título:", livro.titulo);
+console.log("Autor:", livro.autor);
+console.log("Ano de Publicação:", livro.anoPublicacao);
+console.log("Gênero:", livro.genero);
