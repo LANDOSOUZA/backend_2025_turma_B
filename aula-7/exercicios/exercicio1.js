7f4c2651706a2f415352402968976d419c71a3c6
LANDOSOUZA <lando-souza@hotmail.com>
2025-09-24 22:33:59 -0300

    Update exercicio1.js

--- aula-7/exercicios/exercicio1.js
+++ aula-7/exercicios/exercicio1.js
@@ -1,11 +1,13 @@
 /*
-    Criar uma classe chamada carro com os seguintes atributos: marca, modelo, ano,
-    motor ligado
+    Criar uma classe chamada carro com os seguintes atributos: marca, modelo, ano, motor ligado.
+
     Métodos da classe:
     ligar_motor(): Um método que liga o motor do carro e atualiza o atributo motor_ligado
     para True.
+
     desligar_motor(): Um método que desliga o motor do carro e atualiza o atributo
     motor_ligado para False.
+    
     status_motor(): Um método que retorna uma mensagem indicando se o motor está
     ligado ou desligado. Teste sua classe criando um objeto Carro, ligando e desligando o
     motor, e verificando o status do motor.
